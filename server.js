a58fb00676f99c23f0bdc33c4c138c05d1033685
Rohitsharma-007 <rohitsharma271999.rs@gmail.com>
2022-08-29 18:29:42 +0530

    first working server commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true})) 
+app.use(express.static(__dirname+'/public'));
+
+const todolist = ["Milk","Vegitables","Fruits"]
+const workList = []
+app.get("/",(req,res)=>{
+    let today = new Date();
+    let options ={
+        weekday:"long",
+        day : "numeric",
+        month : "long"
+    }
+    let day = today.toLocaleString("en-us",options);
+
+    res.render('index',{listTitle:day,inputValue:todolist});
+});
+
+app.post("/",(req,res)=>{
+   if(req.body.list==="Work-List"){
+       workList.push(req.body.task)
+       res.redirect("/work");
+   }
+   else{
+    todolist.push(req.body.task);
+    res.redirect("/")
+   }
+   
+})
+app.get("/work",(req,res)=>{
+    res.render("index",{listTitle:"Work-List",inputValue:workList})
+})
+
+
+app.listen(8000,()=>console.log("server started on port 8000"))
